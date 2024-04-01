7306b4d6cf6eb989b36d9050f27c1e6df798e54d
CatoshiCodr <martin@dominantstrategies.io>
2024-04-01 22:22:06 +0100

    add config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
+  env: {
+    'jest/globals': true,
+  },
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    quotes: ['error', 'single'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react/default-props-match-prop-types': ['error'],
+    'react/sort-prop-types': ['error'],
+    'prettier/prettier': 1,
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
 };
