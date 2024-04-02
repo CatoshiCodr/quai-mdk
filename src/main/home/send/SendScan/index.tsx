fb35c0e536c4998267b3300697ea8e4461112702
CatoshiCodr <martin@dominantstrategies.io>
2024-04-02 19:50:57 +0100

    Remove Depracated Scanner Code

--- src/main/home/send/SendScan/index.tsx
+++ src/main/home/send/SendScan/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
 import {
-  useQuaiPayCamera,
   QuaiPayCamera,
   QuaiPayContactBottomSheet,
   QuaiPayContent,
-  ScannerType,
 } from 'src/shared/components';
 
 const SendScanScreen: React.FC = () => {
-  const { frameProcessor } = useQuaiPayCamera(ScannerType.SEND_AMOUNT);
 
   return (
     <QuaiPayContent noNavButton hasBackgroundVariant>
-      <QuaiPayCamera frameProcessor={frameProcessor} />
+      <QuaiPayCamera />
       <QuaiPayContactBottomSheet />
     </QuaiPayContent>
   );
